d48bfd17b23b46a5f920882c091030623eb98bc3
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-08-17 17:18:12 -1000

    App.js set up with routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Portfolio from './pages/Portfolio'
+import NotFound from './pages/NotFound'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <main id="main-page">
+          <Navbar />
+
+          <Switch>
+
+            <Route exact path="/">
+              <About />
+            </Route>
+
+            <Route exact path="/portfolio">
+              <Portfolio />
+            </Route>
+
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+
+            <Route exact path="*">
+              <NotFound />
+            </Route>
+
+          </Switch>
+
+        </main>
+      </Router>
+    </>
+    
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+import Hero from '../../components/HeroPic'
+
+function Home() {
+<>
+
+</>
+
+}
+
+export default Home;
