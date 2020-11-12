498b8fdbae94b35e22ccc5ab7c667ed64b52801d
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-11-11 14:52:36 -1000

    about me card updated

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -7,7 +7,10 @@ function About() {
         <>
         <HeroPic text="Andrew Bergstrom" />
         <div >
-        <AboutMeCard className='bio' img='./images/profilepic.jpg' title="About Me" description="As a full stack web developer with a background in biology I am looking to incorporate my skills as a developer into the natural world. Having graduated from University of Washington’s full stack web development bootcamp I have gained more experience and understanding of multiple front-end languages and libraries such as HTML/CSS/JavaScript, JQuery, Handlebars. On the back-end I have experience with languages such as: Express, Node.js, User Authentication, API Interaction with AJAX and JSON, MySQL, MongoDB and Mongoose. I have excellent communication and teamwork skills, am highly  organized and eager to contribute, learn and lead with any team.  
+        <AboutMeCard className='bio' img='./images/profilepic.jpg' title="About Me" description="An aspiring Full Stack Web Developer. I have years of experience demonstrating excellent communication and teamwork skills while being highly organized and eager to contribute, learn and lead with any team. Having graduated from University of Washington’s full stack web development bootcamp, I have experience and understanding of multiple front-end languages and libraries such as HTML/CSS/JavaScript, JQuery, Handlebars. On the back-end I have experience with languages such as:Express, Node.js, User Authentication, API Interaction with AJAX and JSON, MySQL, MongoDB and Mongoose. 
+
+Co-workers of mine have always complimented me on my desire to learn, and my ability to be comfortable in a fast paced, ever-changing environment.
+ 
         " github="https://github.com/AndrewBergstrom" linkedin='https://www.linkedin.com/in/andrew-bergstrom-74a79aa4/' />
     </div>
         </>
