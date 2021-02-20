a9ab95c4738bd493966d8f3fa0316d70576a601d
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-02-19 14:37:34 -1000

    bio update

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -7,11 +7,13 @@ function About() {
         <>
         <HeroPic text="Andrew Bergstrom" />
         <div >
-        <AboutMeCard className='bio' img='./images/profilepic.jpg' title="About Me" description="An aspiring Full Stack Web Developer. I have years of experience demonstrating excellent communication and teamwork skills while being highly organized and eager to contribute, learn and lead with any team. Having graduated from University of Washington’s full stack web development bootcamp, I have experience and understanding of multiple front-end languages and libraries such as HTML/CSS/JavaScript, JQuery, Handlebars. On the back-end I have experience with languages such as:Express, Node.js, User Authentication, API Interaction with AJAX and JSON, MySQL, MongoDB and Mongoose. 
+        <AboutMeCard className='bio' img='./images/profilepic.jpg' title="About Me" description="Rising full stack web developer. I have years of experience demonstrating excellent communication and teamwork skills while being highly collaborative and eager to contribute, grow and lead. Having graduated from University of Washington’s full stack web development bootcamp, I have experience and understanding of multiple front-end languages and libraries such as HTML/CSS/JavaScript, JQuery, React.JS. I also have a special interest in back-end development using MySQL, API Interaction, MongoDB, and Mongoose.
+
+I'm very passionate about the natural world and technology, currently looking for a way to merge my unique skillsets together.
 
 Co-workers of mine have always complimented me on my desire to learn, and my ability to be comfortable in a fast paced, ever-changing environment.
  
-        " github="https://github.com/AndrewBergstrom" linkedin='https://www.linkedin.com/in/andrew-bergstrom-74a79aa4/' />
+        " github="https://github.com/AndrewBergstrom" linkedin='https://www.linkedin.com/in/andrew-bergstrom/' />
     </div>
         </>
     )
